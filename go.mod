c32b375769153131695226ceb11a6a05c974e640
Timothy <timothylam96@hotmail.com>
2022-09-18 21:00:02 +0800

    update example with integer and float

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/timothyl96/goo-example
 
 go 1.19
 
-require github.com/timothyl96/goo v0.0.0-20220917193352-e01f8b9f9ed9
+require github.com/timothyl96/goo v0.0.0-20220918124657-116e8e2b3a44
--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
 )
 
 func main() {
-	// Using var
+	// ##### String
+	// 1. Using var
 	var a goo.String
 	a = "abc"
 	a = a.ToUpper()
 	a = a[1:]
 	fmt.Println(a) // Prints BC
 
-	// Using new function
-	b := goo.NewString("aBc")
+	// 2. Using from function
+	b := goo.FromString("aBc")
 	b = b.ToUpper()
 	fmt.Println(b) // Prints ABC
 
@@ -24,4 +25,14 @@ func main() {
 
 	d := b.ToLower()
 	fmt.Println(d) // Prints abc
+
+	// ##### Integer
+	i1 := goo.FromInt32(123)
+	fmt.Println(i1)           // Prints 123
+	fmt.Println(i1.ToInt32()) // Prints 123
+
+	// ##### Float
+	f1 := goo.FromFloat64(123.232)
+	fmt.Println(f1)             // Prints 123
+	fmt.Println(f1.ToFloat64()) // Prints 123
 }
